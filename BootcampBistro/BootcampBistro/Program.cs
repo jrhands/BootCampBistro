26b229a2b766d01632e7b2403969caa2eec53029
Ross Hinman <hinman.ross@gmail.com>
2018-08-02 11:26:28 -0400

    Menu List

--- BootcampBistro/BootcampBistro/Program.cs
+++ BootcampBistro/BootcampBistro/Program.cs
@@ -10,6 +10,22 @@ namespace BootcampBistro
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("== Welcome to the Bootcamp Bistro! ==");
+            Console.WriteLine("  {0,-20} {1,10}", "Item", "Price");
+            Console.WriteLine("=====================================");
+            Console.WriteLine("  {0,-20} {1,10}", "Roast Beef Sandwich", "$12.99");
+            Console.WriteLine("  {0,-20} {1,10}", "Turkey Sandwich", "$12.99");
+            Console.WriteLine("  {0,-20} {1,10}", "Ham Sandwich", "$10.99");
+            Console.WriteLine("  {0,-20} {1,10}", "Club", "$10.99");
+            Console.WriteLine("  {0,-20} {1,10}", "Tuna Sandwich", "$12.99");
+            Console.WriteLine("  {0,-20} {1,10}", "Veggie Sandwich", "$10.99");
+            Console.WriteLine("  {0,-20} {1,10}", "Italian Sandwich", "$11.99");
+            Console.WriteLine("  {0,-20} {1,10}", "Burger", "$13.99");
+            Console.WriteLine("  {0,-20} {1,10}", "Hot Dog", "$7.99");
+            Console.WriteLine("  {0,-20} {1,10}", "Potatoe Chips", "$2.99");
+            Console.WriteLine("  {0,-20} {1,10}", "BBQ Chips", "$2.99");
+            Console.WriteLine("=====================================");
+
         }
     }
 }
